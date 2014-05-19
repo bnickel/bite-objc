0c82e23c3459f545300e25777fbdfb7a85394f6f
Brian Nickel <bnickel@stackexchange.com>
2014-05-19 11:58:09 -0700

    Test laziness and skipping.

--- BiteTests/BiteTests.m
+++ BiteTests/BiteTests.m
@@ -31,6 +31,10 @@
     [super tearDown];
 }
 
+#pragma mark - Transforms
+
+#pragma mark take:
+
 - (void)testThatTakeLimitsTheAmountTaken
 {
     XCTAssertEqual(10, [[[BITE(primes) take:10] array] count], @"Take should have limited the count.");
@@ -46,6 +50,23 @@
     
     items = [[BITE(primes) take:19] array];
     XCTAssertEqual(items.count, primes.knownPrimes.count, @"The enumerator should have stopped after %lu calls.", (unsigned long)items.count);
+}
+
+#pragma mark skip:
+
+- (void)testSkip
+{
+    NSArray *items = [[[BITE(primes) skip:15] take:5] array];
+    XCTAssertEqual(20, primes.knownPrimes.count, @"Should have iterated over 20 elements.");
+    XCTAssertEqualObjects(items, [primes.knownPrimes subarrayWithRange:NSMakeRange(15, 5)], @"Subarray should match.");
+}
+
+#pragma mark - Laziness
+
+- (void)testExecutionDelay
+{
+    [[[BITE(primes) filter:^BOOL(id obj) {return [[obj description] hasSuffix:@"1"];}] skip:10000] take:10000];
+    XCTAssertEqual(primes.knownPrimes.count, 1, @"Should not have iterated.");
 }
 
 @end
